f52444e93076a54b2b8e554fb72194fb5fbf91c0
NTDyn <duyenthuy843@gmail.com>
2023-10-27 16:18:36 +0700

    lsp

--- src/DTO/loaiSanPhamDTO.java
+++ src/DTO/loaiSanPhamDTO.java
@@ -1,17 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package DTO;
 
+/**
+ *
+ * @author trant
+ */
 public class loaiSanPhamDTO {
     private int maLoaiSanPham;
-    private String tenLoaiSanPham;
-
-    public loaiSanPhamDTO() {
-    }
-
+    private String tenLoaiSanPham ;
 
     public loaiSanPhamDTO(int maLoaiSanPham, String tenLoaiSanPham) {
         this.maLoaiSanPham = maLoaiSanPham;
         this.tenLoaiSanPham = tenLoaiSanPham;
-       
+    }
+
+    public loaiSanPhamDTO() {
     }
 
     public int getMaLoaiSanPham() {
@@ -29,6 +35,6 @@ public class loaiSanPhamDTO {
     public void setTenLoaiSanPham(String tenLoaiSanPham) {
         this.tenLoaiSanPham = tenLoaiSanPham;
     }
-
+    
     
 }
