4596e0269733435418dcee781152abf99e4f0bb7
Anhdeptroai <nguyentrunganh30092003@gmail.com>
2023-11-17 19:03:16 +0700

    Update file ChangePassword

--- src/DAO/NguoiDungDAO.java
+++ src/DAO/NguoiDungDAO.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+//import model.Account;
 
 public class NguoiDungDAO {
     public NguoiDungDAO() {
@@ -57,6 +58,29 @@ public class NguoiDungDAO {
         return user;
     }
     
+    //@Override
+    public int update(NguoiDungDTO t) {
+        int ketQua = 0;
+        try {
+            Connection con = JDBCUtil.getConnection();
+            String sql = "UPDATE nguoidung SET matkhau=?, hoten=?, email=?, manhomquen=? trangthai=? WHERE userName=?";
+            PreparedStatement pst = con.prepareStatement(sql);          
+            pst.setString(1, t.getMatKhau());
+            pst.setString(2, t.getHoTen());
+            pst.setString(3, t.getEmail());
+            pst.setInt(4, t.getMaNhomQuyen());
+            pst.setInt(5, t.getTrangThai());
+            pst.setString(6, t.getTaiKhoan());
+            ketQua = pst.executeUpdate();
+            JDBCUtil.closeConnection(con);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+        return ketQua;
+    }
+    
     public int changePassword(NguoiDungDTO user, String newPassword) throws SQLException {
         int result = 0;
         try {
@@ -64,6 +88,22 @@ public class NguoiDungDAO {
             String query = "UPDATE nguoidung SET matkhau = ? WHERE taikhoan = ?";
             PreparedStatement ps = conn.prepareStatement(query);
             ps.setString(1, newPassword);
+            ps.setString(2, user.getTaiKhoan());
+            result = ps.executeUpdate();
+            JDBCUtil.closeConnection(conn);
+        } catch (SQLException e) {
+            throw e;
+        }
+        return result;
+    }
+    
+    public int changeEmail(NguoiDungDTO user, String newEmail) throws SQLException {
+        int result = 0;
+        try {
+            Connection conn = JDBCUtil.getConnection();
+            String query = "UPDATE nguoidung SET email = ? WHERE taikhoan = ?";
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, newEmail);
             ps.setString(2, user.getTaiKhoan());
             result = ps.executeUpdate();
             JDBCUtil.closeConnection(conn);
