ffb9429c8fe7adf1686196f3af916628951314fb
Nhien <nhienau@gmail.com>
2023-12-05 23:44:08 +0700

    Update database file

--- khomaytinh.sql
+++ khomaytinh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2023 at 06:50 PM
+-- Generation Time: Dec 05, 2023 at 05:43 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -130,31 +130,39 @@ INSERT INTO `chitietphieunhap` (`maphieunhap`, `manhacungcap`, `masanpham`, `sol
 (6, 2, 20, 20, 38490000, 'thienan', 20, 0, 1),
 (6, 8, 13, 10, 31690000, 'thienan', 10, 10, 1),
 (7, 6, 4, 10, 21690000, 'baoduy', 10, 0, 1),
-(7, 8, 25, 20, 30790000, 'baoduy', 20, 10, 1),
-(8, 6, 8, 10, 16550000, 'ngoctram', 10, 10, 1),
+(7, 8, 25, 20, 30790000, 'baoduy', 20, 0, 1),
+(8, 6, 8, 10, 16550000, 'ngoctram', 10, 0, 1),
 (9, 2, 8, 20, 16000000, 'baoduy', 20, 0, 1),
 (10, 8, 26, 10, 22890000, 'ngoctram', 10, 0, 1),
 (11, 1, 17, 10, 23090000, 'hoainam', 10, 0, 1),
 (11, 6, 11, 20, 20790000, 'hoainam', 20, 0, 1),
-(12, 5, 11, 20, 21690000, 'thienan', 20, 10, 1),
+(12, 5, 11, 20, 21690000, 'thienan', 20, 0, 1),
 (12, 7, 14, 10, 34790000, 'thienan', 10, 10, 1),
 (13, 6, 18, 10, 16290000, 'baoduy', 10, 10, 1),
-(13, 7, 7, 10, 14200000, 'baoduy', 10, 10, 1),
+(13, 7, 7, 10, 14200000, 'baoduy', 10, 0, 1),
 (13, 7, 14, 10, 34790000, 'baoduy', 10, 0, 1),
 (14, 5, 7, 10, 14200000, 'thienan', 10, 10, 1),
-(15, 4, 17, 10, 24090000, 'ngoctram', 10, 10, 1),
+(15, 4, 17, 10, 24090000, 'ngoctram', 10, 0, 1),
 (15, 5, 7, 20, 14200000, 'ngoctram', 20, 0, 1),
 (16, 2, 13, 10, 32590000, 'thuyduyen', 10, 10, 1),
-(16, 8, 21, 10, 19790000, 'thuyduyen', 10, 10, 1),
+(16, 8, 21, 10, 19790000, 'thuyduyen', 10, 0, 1),
 (17, 7, 14, 10, 34790000, 'thienan', 10, 10, 1),
-(18, 8, 2, 10, 9150000, 'thienan', 8, 10, 1),
-(19, 5, 7, 10, 14200000, 'nvnhap', 0, 10, 1);
+(18, 8, 2, 10, 9150000, 'thienan', 10, 10, 1),
+(19, 5, 7, 10, 14200000, 'nvnhap', 8, 0, 1),
+(20, 1, 19, 10, 12540000, 'thienan', 10, 10, 1),
+(21, 8, 26, 10, 22890000, 'thienan', 10, 10, 1),
+(22, 8, 24, 10, 13790000, 'nvnhap', 0, 0, 1),
+(24, 8, 25, 5, 30790000, 'thienan', 5, 5, 1),
+(24, 8, 26, 5, 22890000, 'thienan', 5, 5, 1),
+(25, 6, 3, 15, 18990000, 'thienan', 10, 10, 1),
+(26, 8, 2, 10, 9150000, 'nvnhap', 5, 5, 1),
+(27, 2, 21, 10, 19290000, 'nvnhap', 0, 0, 1);
 
 --
 -- Triggers `chitietphieunhap`
 --
 DELIMITER $$
-CREATE TRIGGER `update_sanpham_soluong` AFTER UPDATE ON `chitietphieunhap` FOR EACH ROW BEGIN
+CREATE TRIGGER `increase_product_quantity` AFTER UPDATE ON `chitietphieunhap` FOR EACH ROW BEGIN
     DECLARE updated_soluong INT;
     DECLARE masanpham_val INT;
     
@@ -162,9 +170,11 @@ CREATE TRIGGER `update_sanpham_soluong` AFTER UPDATE ON `chitietphieunhap` FOR E
     SET updated_soluong = NEW.soluongtonkho;
     SET masanpham_val = NEW.masanpham;
     
-    -- Update the soluong column in the sanpham table
-    UPDATE sanpham SET soluong = soluong + (OLD.soluongtonkho - updated_soluong)
-    WHERE masanpham = masanpham_val;
+    IF updated_soluong < OLD.soluongtonkho THEN
+        -- Update the soluong column in the sanpham table
+        UPDATE sanpham SET soluong = soluong + (OLD.soluongtonkho - updated_soluong)
+        WHERE masanpham = masanpham_val;
+    END IF;
 END
 $$
 DELIMITER ;
@@ -209,7 +219,28 @@ INSERT INTO `chitietphieuxuat` (`maphieuxuat`, `masanpham`, `soluong`, `dongia`)
 (17, 14, 1, 40490000),
 (18, 8, 10, 18490000),
 (19, 14, 7, 40490000),
-(20, 14, 10, 40490000);
+(20, 14, 10, 40490000),
+(21, 7, 10, 14890000),
+(22, 11, 10, 23990000),
+(22, 17, 10, 27490000),
+(23, 25, 10, 35990000),
+(24, 7, 8, 14890000),
+(25, 21, 10, 21890000);
+
+--
+-- Triggers `chitietphieuxuat`
+--
+DELIMITER $$
+CREATE TRIGGER `decrease_product_quantity` AFTER INSERT ON `chitietphieuxuat` FOR EACH ROW BEGIN
+    DECLARE soluongxuat INT;
+    DECLARE masanphamxuat INT;
+    SET masanphamxuat = NEW.masanpham;
+    SET soluongxuat = NEW.soluong;
+    
+    UPDATE sanpham SET soluong = soluong - soluongxuat WHERE masanpham = masanphamxuat;
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -221,7 +252,7 @@ CREATE TABLE `chitietquyen` (
   `manhomquyen` int(11) NOT NULL,
   `machucnang` varchar(50) NOT NULL,
   `hanhdong` varchar(50) NOT NULL,
-  `hanche` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL DEFAULT '[]'
+  `hanche` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -233,6 +264,10 @@ INSERT INTO `chitietquyen` (`manhomquyen`, `machucnang`, `hanhdong`, `hanche`) V
 (1, 'taikhoan', 'delete', '[]'),
 (1, 'taikhoan', 'update', '[]'),
 (1, 'taikhoan', 'view', '[]'),
+(2, 'kho', 'create', '[]'),
+(2, 'kho', 'delete', '[]'),
+(2, 'kho', 'update', '[]'),
+(2, 'kho', 'view', '[]'),
 (2, 'loaisanpham', 'create', '[]'),
 (2, 'loaisanpham', 'delete', '[]'),
 (2, 'loaisanpham', 'update', '[]'),
@@ -253,16 +288,16 @@ INSERT INTO `chitietquyen` (`manhomquyen`, `machucnang`, `hanhdong`, `hanche`) V
 (2, 'taikhoan', 'delete', '[]'),
 (2, 'taikhoan', 'view', '[]'),
 (2, 'thongke', 'view', '[]'),
-(2, 'tonkho', 'create', '[]'),
-(2, 'tonkho', 'delete', '[]'),
 (2, 'tonkho', 'update', '[]'),
 (2, 'tonkho', 'view', '[]'),
+(3, 'kho', 'view', '[]'),
 (3, 'loaisanpham', 'view', '[]'),
 (3, 'nhacungcap', 'view', '[]'),
 (3, 'phieunhap', 'create', '[\"nhacungcap\", \"gianhap\"]'),
 (3, 'phieunhap', 'view', '[]'),
 (3, 'sanpham', 'view', '[]'),
 (3, 'tonkho', 'view', '[\"gianhap\"]'),
+(4, 'kho', 'view', '[]'),
 (4, 'loaisanpham', 'view', '[]'),
 (4, 'phieuxuat', 'create', '[]'),
 (4, 'phieuxuat', 'view', '[]'),
@@ -286,6 +321,7 @@ CREATE TABLE `chucnang` (
 --
 
 INSERT INTO `chucnang` (`machucnang`, `tenchucnang`, `trangthai`) VALUES
+('kho', 'Quản lý kho', 1),
 ('loaisanpham', 'Quản lý loại sản phẩm', 1),
 ('nhacungcap', 'Quản lý nhà cung cấp', 1),
 ('phieunhap', 'Quản lý phiếu nhập', 1),
@@ -313,10 +349,10 @@ CREATE TABLE `kho` (
 --
 
 INSERT INTO `kho` (`makho`, `tenkho`, `diachi`, `trangthai`) VALUES
-(1, 'a', 'q5', 1),
-(2, 'b', 'q10', 1),
-(3, 'c', 'q3', 1),
-(4, 'd', 'q1', 1);
+(1, 'Kho Q1 TP.HCM', '65/13 Trần Đình Xu, Phường Cầu Kho, Quận 1, TP.HCM', 1),
+(2, 'Kho Hà Nội', '412 Lạc Long Quân, Quận Tây Hồ, Hà Nội', 1),
+(3, 'Kho Hải Phòng', '3 D22, Phường Đồng Quốc Bình, Quận Ngô Quyền, TP. Hải Phòng', 1),
+(4, 'Kho Q. Bình Thạnh TP.HCM', '292 Nơ Trang Long, Phường 12, Quận Bình Thạnh, TP.HCM', 1);
 
 -- --------------------------------------------------------
 
@@ -364,18 +400,18 @@ CREATE TABLE `nguoidung` (
 --
 
 INSERT INTO `nguoidung` (`taikhoan`, `matkhau`, `hoten`, `email`, `manhomquyen`, `trangthai`) VALUES
-('baoduy', '$2a$12$K/ENOCA71OHp7z3XfSP1MutwfEOCfaeTZ6TA8WhIQD90ptnuUORjW', 'Nguyễn Lê Bảo Duy', 'baoduy@gmail.com', 2, 1),
-('haonhien', '$2a$12$l/LtGoIRthw8C1Ol2KB4k.Dal/1jC9YszPcKeIK3P0G5.OywK0oYK', 'Âu Hạo Nhiên', 'nhienau@gmail.com', 1, 1),
-('hoainam', '$2a$12$0rHGEBziwTtk29BSlwmju.R0OuvKu8Oc/LoI1O5hvQPuANbQfYAwC', 'Thân Trọng Hoài Nam', 'hoainam@gmail.com', 2, 1),
-('ngoctram', '$2a$12$LIrqqUIO.S.UqZyj1i0Dp.klvpvhUb/4V40hGKfkoNrs4xMfJk7cW', 'Võ Hồ Ngọc Trâm', 'ngoctram@gmail.com', 2, 1),
-('nvnhap', '$2a$12$vK9g/aY1nPKvpEASL7fzeugVAHdm281zb8akeHO4I1g7sdE/pH7Wm', 'Test nhan vien nhap', 'nvnhap@gmail.com', 3, 1),
-('nvxuat', '$2a$12$kiRyqK5HypKi.n4CpbS3PO6lFXcZRdtyEMO96HhrY4TAYOKQwSfJq', 'Test nhan vien xuat', 'nvxuat@gmail.com', 4, 1),
-('testuser1', '$2a$12$uDmFamJCdeTYHhKqzQE6nu3e03gSCqXpaX7Lk1Ojx8GUT6ytIUjd.', 'Test', 'test@gmail.com', 3, 0),
-('testuser2', '$2a$12$IfC/L/ozLxiwphaukOn3Z.VNELcDshk6/kfRnaXoYvD8ZCWKVH2nu', 'Test2', 'testuser2@gmail.com', 5, 1),
-('testuser3', '$2a$12$SioTgn2/CrEBTPTetXYVfemL/ITp12PT0OlKLnLuFeCbm1/qTqKfy', 'Test3', 'testuser3@gmail.com', 4, 1),
-('thienan', '$2a$12$phthu7KkJNlcmJ0oRfP1SOoFcAu2murqtpOD1XA9mgDCnLL4JfSRm', 'Nguyễn Thiên Ân', 'thienan@gmail.com', 2, 1),
-('thuyduyen', '$2a$12$q/i1Ap90Go4/sIdz1s3ZAOYhCAOMZDXIIi45QMIDDHOzDWcC/P7cW', 'Nguyễn Thuỳ Duyên', 'thuyduyen@gmail.com', 2, 1),
-('trunganh', '$2a$12$5b64FYmh9Gz4DAU.qQqPt.3svz7QsS3WDHMakRgKB4xugxMzxC2ce', 'Nguyễn Trung Anh', 'trunganh@gmail.com', 2, 1);
+('baoduy', '$2a$12$PzpNeXoGH.MPBTCj5CoODekfzNz2ji2fb58NAhzgTeT5KIimwPgkm', 'Nguyễn Lê Bảo Duy', 'baoduy@gmail.com', 2, 1),
+('haonhien', '$2a$12$8ZKIYfNbm61PsTv3vnMFiO82bn7I4KuyMpCcTdSQWWQiFaijVA4Z6', 'Âu Hạo Nhiên', 'nhienau@gmail.com', 1, 1),
+('hoainam', '$2a$12$s6pNzMuvZvnhxzlg4tcWiuMgelOHjzfX4JLC5.QCCBuJjm5kO8p7e', 'Thân Trọng Hoài Nam', 'hoainam@gmail.com', 2, 1),
+('ngoctram', '$2a$12$IRl.BXJrRIt2J5rux9V4s.ZoSxsQLpaITPSKTYTjFicLCAP1RR0ue', 'Võ Hồ Ngọc Trâm', 'ngoctram@gmail.com', 2, 1),
+('nvnhap', '$2a$12$eVl09v71cYD5PB4C7eDoHu7phOu5FwVOsfu2h2bNXq9Z9qGjadtTC', 'Nguyễn Anh Duy', 'nvnhap@gmail.com', 3, 1),
+('nvxuat', '$2a$12$0pgPE.PDGFfdY7.qiCXk2eMmNUwSXQpvgtqepOiufLf5c5eGxh1HS', 'Dương Tiến Ðức', 'nvxuat@gmail.com', 4, 1),
+('testuser1', '$2a$12$AjXYGmuTP4ybCDRMm4.XPOR/3uRKdifqvwFBWAFulihyrI7DqdRzK', 'Ngô Lâm Vũ', 'test@gmail.com', 3, 0),
+('testuser2', '$2a$12$8xi0pseEda5ofRH70nH2DedjWtdjufzoo7EdmNpKWeBaHCxBL51MO', 'Kim Quang Đông', 'testuser2@gmail.com', 5, 1),
+('testuser3', '$2a$12$3QfUiAHfBXV4uBpdv6u9AuXHz4w18ab/Uw02Dj7fBRmJDEUt1JVQS', 'Trần Đức Duy', 'testuser3@gmail.com', 4, 1),
+('thienan', '$2a$12$BQ3I/AQ4CrxTWEl.qscdken6AOUxYgXt0jRVSV0vREsjhGRUpkIOS', 'Nguyễn Thiên Ân', 'thienan@gmail.com', 2, 1),
+('thuyduyen', '$2a$12$Zz/ScTqeWvuBorhHYF1XpOKD8g0XhFHX81AGEFbMdIlUJYVFyic4u', 'Nguyễn Thuỳ Duyên', 'thuyduyen@gmail.com', 2, 1),
+('trunganh', '$2a$12$hGpO.9TAkYJBVlTJ0LTdvu4ykf.NAL.jHjSDQwKUqN1qD4b8xcNOK', 'Nguyễn Trung Anh', 'trunganh@gmail.com', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -399,10 +435,10 @@ INSERT INTO `nhacungcap` (`manhacungcap`, `tennhacungcap`, `sdt`, `diachi`, `tra
 (1, 'Công Ty TNHH Điều Khiển Tự Động An Phát', '02835109735', '86/21 Phan Tây Hồ, P. 7, Q. Phú Nhuận TP. Hồ Chí Minh', 1),
 (2, 'Công Ty TNHH Thương Mại Dịch Vụ Hoàng Cố Đô', '02838115345', '622/16/5 Cộng Hòa, Phường 13, Quận Tân Bình, TP HCM', 1),
 (3, 'Công Ty Cổ Phần Bán Lẻ Kỹ Thuật Số FPT', '02873023456', '261 - 263 Khánh Hội, P2, Q4, TP. Hồ Chí Minh', 1),
-(4, 'Công ty Cổ phần đầu tư công nghệ HACOM', '1900 1903', 'Số 129 - 131, phố Lê Thanh Nghị, Phường Đồng Tâm, Quận Hai Bà Trưng, Hà Nội', 1),
+(4, 'Công ty Cổ phần đầu tư công nghệ HACOM', '19001903', 'Số 129 - 131, phố Lê Thanh Nghị, Phường Đồng Tâm, Quận Hai Bà Trưng, Hà Nội', 1),
 (5, 'Công Ty TNHH Thương Mại Hoàng Phát Hải Phòng', '02253250888', 'Số 4, Lô 2A Lê Hồng Phong, Ngô Quyền, Tp. Hải Phòng', 1),
 (6, 'Công ty cổ phần dịch vụ - thương mại Phong Vũ', '0967567567', 'Tầng 5, Số 117-119-121 Nguyễn Du, Phường Bến Thành, Quận 1, Thành Phố Hồ Chí Minh', 1),
-(7, 'Công ty cổ phần Thế Giới Di Động', '028 38125960', '128 Trần Quang Khải, P. Tân Định, Q.1, TP.Hồ Chí Minh', 1),
+(7, 'Công ty cổ phần Thế Giới Di Động', '02838125960', '128 Trần Quang Khải, P. Tân Định, Q.1, TP.Hồ Chí Minh', 1),
 (8, 'Công ty CP Công nghệ Thương mại Dịch vụ Vietstars', '090 469 0212', ' Số 109 Lê Thanh Nghị, TP Hải Dương', 1);
 
 -- --------------------------------------------------------
@@ -468,8 +504,15 @@ INSERT INTO `phieunhap` (`maphieunhap`, `thoigiantao`, `makho`, `nguoitao`, `ngu
 (15, '2023-10-25 00:18:24', 4, 'ngoctram', 'ngoctram', 'ngoctram', 524900000, 4),
 (16, '2023-10-25 00:18:24', 2, 'thuyduyen', 'thuyduyen', 'thuyduyen', 523800000, 4),
 (17, '2023-11-25 00:39:57', 1, 'thienan', 'thienan', 'thienan', 347900000, 4),
-(18, '2023-12-04 00:00:00', 3, 'thienan', NULL, NULL, 9150000, 4),
-(19, '2023-12-04 00:00:00', 1, 'nvnhap', 'thienan', NULL, 14200000, 3);
+(18, '2023-12-04 00:00:00', 3, 'thienan', 'thienan', 'nvnhap', 91500000, 4),
+(19, '2023-12-04 00:00:00', 1, 'nvnhap', 'thienan', 'nvnhap', 113600000, 4),
+(20, '2023-12-05 00:00:00', 4, 'thienan', 'thienan', 'nvnhap', 125400000, 4),
+(21, '2023-12-05 00:00:00', 4, 'thienan', 'thienan', 'nvnhap', 228900000, 4),
+(22, '2023-12-05 00:00:00', 1, 'nvnhap', NULL, NULL, 137900000, 2),
+(24, '2023-12-05 00:00:00', 1, 'thienan', 'thienan', 'nvnhap', 268400000, 4),
+(25, '2023-12-05 00:00:00', 1, 'thienan', 'thienan', 'nvnhap', 189900000, 4),
+(26, '2023-12-05 00:00:00', 1, 'nvnhap', 'trunganh', 'nvnhap', 45750000, 4),
+(27, '2023-12-05 00:00:00', 3, 'nvnhap', NULL, NULL, 192900000, 1);
 
 -- --------------------------------------------------------
 
@@ -509,7 +552,12 @@ INSERT INTO `phieuxuat` (`maphieuxuat`, `thoigiantao`, `nguoitao`, `tongtien`, `
 (17, '2023-11-27 09:47:27', 'ngoctram', 40490000, 1),
 (18, '2023-12-05 00:34:24', 'trunganh', 184900000, 1),
 (19, '2023-12-05 00:35:14', 'baoduy', 283430000, 1),
-(20, '2023-12-05 00:44:02', 'thienan', 404900000, 1);
+(20, '2023-12-05 00:44:02', 'thienan', 404900000, 1),
+(21, '2023-12-05 17:27:00', 'thienan', 148900000, 1),
+(22, '2023-12-05 17:30:15', 'ngoctram', 514800000, 1),
+(23, '2023-12-05 17:59:20', 'trunganh', 359900000, 1),
+(24, '2023-12-05 23:33:38', 'ngoctram', 119120000, 1),
+(25, '2023-12-05 23:37:12', 'ngoctram', 218900000, 1);
 
 -- --------------------------------------------------------
 
@@ -546,8 +594,8 @@ INSERT INTO `sanpham` (`masanpham`, `maloaisanpham`, `tensanpham`, `soluong`, `g
 (4, 2, 'MSI Katana 15 B13VEK 1205VN', 0, 24490000, 'Intel Core i5-13420H (3.40GHz~4.60GHz) 8 Cores 12 Threads, 12 MB Intel® Smart Cache', '8GB (1 x 8GB) DDR5 5200MHz (2x SO-DIMM socket, up to 64GB SDRAM)', 'NVIDIA GeForce RTX 4050 Laptop GPU 6GB GDDR6 + MUX Switch', '512GB NVMe PCIe Gen 4 SSD (2 slots)', '15.6\" FHD (1920x1080), 144Hz, IPS-Level, 45% NSTC', '3 cell, 53.5Whr', 2.25, 'Đen', 'Windows 11 Home', 1),
 (5, 4, 'Acer Predator Helios 300 PH315 55 76KG', 0, 33990000, 'Intel® Core™ i7-12700H (up to 4.7Ghz, 24MB cache)', '16GB DDR5 4800Mhz (2x8GB) (2x SO-DIMM socket, up to 32GB SDRAM)', 'NVIDIA GeForce RTX 3060 6GB GDDR6', '512GB NVMe PCIe Gen3x4 SSD (2 slot)', '15.6 inch QHD (2560 x1440) IPS 165Hz, DCI-P3 100%, 5ms, 300nits, SlimBezel', '4 cell, 90Whr', 2.4, 'Đen', 'Windows 11 Home', 1),
 (6, 8, 'LG Gram 2023 16Z90R GAH76A5', 0, 44490000, 'Intel Core i7-1360P (12 Cores: 4P + 8E, P: 2.2 up to 5.0 GHz / E: 1.6 up to 3.7 GHz) 18 MB Cache', '16GB LPDDR5 6000MHz (Dual Channel, Onboard, không nâng cấp)', 'Intel Iris Xe Graphics', '512GB PCIe NVMe M.2 SSD (2 slot, còn trống 1 khe M.2)', '16 inch WQXGA (2560x1600), 16:10, IPS 60Hz, DCI-P3 99%, LGD, 350 nits, Anti-Glare', '80WHr', 1.199, 'Xám', 'Windows 11 Home', 1),
-(7, 3, 'Asus VivoBook 14 OLED M1405YA KM047W', 0, 14890000, 'Ryzen™ 5 7530U Mobile Processor (6-core/12-thread, 16MB cache, up to 4.3 GHz max boost)', '8GB Onboard DDR4 3200MHz (1 x Sodimm Slot, up to 24GB Ram)', 'AMD Radeon™ Graphics', '512GB M.2 NVMe™ PCIe® 3.0 SSD (1 slot, support M.2 2280 PCIe 3.0x4)', '14\" 2.8K (2880 x 1800) OLED 16:10, 90Hz 0.2ms, 600nits, 100% DCI-P3, Glossy display, Screen-to-body ratio: 86%, PANTONE Validated, VESA CERTIFIED Display HDR True Black 600', '50WHrs, 3S1P, 3-cell Li-ion', 1.6, 'Cool Silver', 'Windows 11 Home', 1),
-(8, 3, 'Asus VivoBook 15X OLED S3504VA L1226W', 0, 18490000, 'Intel® Core™ i5-1340P Processor 1.9 GHz (12MB Cache, up to 4.6 GHz, 12 cores, 16 Threads)', '16GB (8GB Onboard + 8GB Sodimm) DDR4 3200MHz', 'Intel Iris Xe Graphics (with dual channel memory)', '512GB M.2 NVMe™ PCIe® 4.0 SSD', '15.6\" FHD (1920 x 1080) OLED 16:9 aspect ratio, 60Hz 0.2ms, 550nits HDR peak brightness, 100% DCI-P3, Glossy display, Screen-to-body ratio: 85%, PANTONE Validated, VESA CERTIFIED Display HDR True Black 500', '50WHrs, 3S1P, 3-cell Li-ion', 1.7, 'Cool Silver', 'Windows 11 Home', 1),
+(7, 3, 'Asus VivoBook 14 OLED M1405YA KM047W', -8, 14890000, 'Ryzen™ 5 7530U Mobile Processor (6-core/12-thread, 16MB cache, up to 4.3 GHz max boost)', '8GB Onboard DDR4 3200MHz (1 x Sodimm Slot, up to 24GB Ram)', 'AMD Radeon™ Graphics', '512GB M.2 NVMe™ PCIe® 3.0 SSD (1 slot, support M.2 2280 PCIe 3.0x4)', '14\" 2.8K (2880 x 1800) OLED 16:10, 90Hz 0.2ms, 600nits, 100% DCI-P3, Glossy display, Screen-to-body ratio: 86%, PANTONE Validated, VESA CERTIFIED Display HDR True Black 600', '50WHrs, 3S1P, 3-cell Li-ion', 1.6, 'Cool Silver', 'Windows 11 Home', 1),
+(8, 3, 'Asus VivoBook 15X OLED S3504VA L1226W', 10, 18490000, 'Intel® Core™ i5-1340P Processor 1.9 GHz (12MB Cache, up to 4.6 GHz, 12 cores, 16 Threads)', '16GB (8GB Onboard + 8GB Sodimm) DDR4 3200MHz', 'Intel Iris Xe Graphics (with dual channel memory)', '512GB M.2 NVMe™ PCIe® 4.0 SSD', '15.6\" FHD (1920 x 1080) OLED 16:9 aspect ratio, 60Hz 0.2ms, 550nits HDR peak brightness, 100% DCI-P3, Glossy display, Screen-to-body ratio: 85%, PANTONE Validated, VESA CERTIFIED Display HDR True Black 500', '50WHrs, 3S1P, 3-cell Li-ion', 1.7, 'Cool Silver', 'Windows 11 Home', 1),
 (9, 7, 'Dell Inspiron 15 3520 N5I5122W1 Black', 0, 15990000, 'Intel(R) Core(TM) i5-1235U Processor (12MB Cache, up to 4.4 GHz)', '1 x 8GB DDR4 2666MHz (2x SO-DIMM socket, up to 16GB SDRAM)', 'Intel Iris Xe Graphics (with dual channel memory); Intel® UHD Graphics', '256GB SSD NVMe PCIe (1 Slot)', '15.6 Inch FHD (1920 x 1080),120Hz, Anti- Glare LED Backlit Narrow Border Display', '3-cell Li-ion, 41 Wh', 1.9, 'Carbon Black', 'Windows 11 Home + Office Home&Student', 1),
 (10, 7, 'Dell Vostro 3520 V5I3614W1 Gray', 0, 12490000, 'Intel Core i3 - 1215U (Up to 4.4 Ghz, 12Mb)', '8GB (8x1) DDR4 2666MHz (2x SO-DIMM socket, up to 16GB SDRAM)', 'Intel UHD Graphics', '256GB SSD M.2 PCIE', '15.6 inch FHD (1920 x 1080) 120Hz 250 nits WVA Anti- Glare LED Backlit Narrow Border Display', '3 Cell 41WHr', 1.66, 'Đen', 'Windows 11 Home + Office Home & Student 2021', 1),
 (11, 1, 'Lenovo ThinkPad E14 21E300E3VN', 0, 23990000, 'Intel® Core™ i7-1255U, 10 Cores (2P + 8E) / 12 Threads, P-core 1.7 / 4.7GHz, E-core 1.2 / 3.5GHz, 12MB', '8GB Soldered DDR4-3200 (Trống 1 slot Sodimm, nâng cấp tối đa 40GB)', 'Intel Iris Xe Graphics (with dual channel memory); Intel® UHD Graphics (with single channel memory)', '512GB SSD M.2 2242 PCIe® 4.0x4 NVMe® Opal 2.0 (Còn trống 1 Slot M.2 2242 PCIe 3.0 x4)', '14\" FHD (1920x1080) IPS 300nits Anti-glare, 45% NTSC', '45Whr battery', 1.64, 'Black', 'Windows 11 Home', 1),
@@ -723,13 +771,13 @@ ALTER TABLE `nhomquyen`
 -- AUTO_INCREMENT for table `phieunhap`
 --
 ALTER TABLE `phieunhap`
-  MODIFY `maphieunhap` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `maphieunhap` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT for table `phieuxuat`
 --
 ALTER TABLE `phieuxuat`
-  MODIFY `maphieuxuat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `maphieuxuat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `sanpham`
